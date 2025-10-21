ab464a690c6039c903460ef307a4788aa9c26c6c
susifitrianti29-cmyk <susifitrianti29@gmail.com>
2025-10-21 10:14:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,46 +2,57 @@
 <html>
 <head>
     <title>Buku Tamu Digital</title>
-     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <center><h1>BUKU TAMU</h1></center>
-   <center> <p>DINAS KOMUNIKASI DAN INFORMATIKA<br>KABUPATEN BELITUNG</p></center>
-  </div>
-</div>
-    <div class="form-wrapper">
-    <h2>Buku Tamu Digital 2025</h2>
-     <form action="simpan.php" method="post" enctype="multipart/form-data">
-        <div class="form-row">
-         Nama: <input type="text" name="nama" required><br>
-        </div>
-         
-            <div class="form-row">
-        Instansi: <input type="text" name="instansi"><br>
-            </div>
-         
+    <div class="container">
+        <header>
+            <h1>Buku Tamu Digital</h1>
+            <p>DINAS KOMUNIKASI DAN INFORMATIKA KABUPATEN BELITUNG</p>
+        </header>
+
+        <div class="form-wrapper">
+            <h2>Formulir Data Tamu</h2>
+            <form action="proses_buku_tamu.php" method="post" enctype="multipart/form-data">
                 <div class="form-row">
-        Alamat: <input type="text" name="alamat"><br>
+                    <label for="nama">Nama:</label>
+                    <input type="text" id="nama" name="nama" required>
                 </div>
-         
-                    <div class="form-row">
-        No HP: <input type="text" name="no_hp"><br>
-                    </div>
-         
-                        <div class="form-row">
-        Email: <input type="email" name="email"><br>
-                        </div>
-                            <div class="form-row">
-        Keperluan: <input type="text" name="keperluan"><br>
-                            </div>
-         
-                                <div class="form-row">
-        Tanggal Kunjungan: <input type="date" name="tanggal_kunjungan" required><br>
-                                </div>
-        <!-- Tanda Tangan (upload gambar): <input type="file" name="ttd_digital" accept="image/*"><br>
-        Foto (upload gambar): <input type="file" name="foto" accept="image/*"><br> -->
-        <input type="submit" value="Kirim">
-    </form>
+
+                <div class="form-row">
+                    <label for="instansi">Instansi:</label>
+                    <input type="text" id="instansi" name="instansi">
+                </div>
+
+                <div class="form-row">
+                    <label for="alamat">Alamat:</label>
+                    <input type="text" id="alamat" name="alamat">
+                </div>
+
+                <div class="form-row">
+                    <label for="no_hp">No HP:</label>
+                    <input type="text" id="no_hp" name="no_hp">
+                </div>
+
+                <div class="form-row">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email">
+                </div>
+
+                <div class="form-row">
+                    <label for="keperluan">Keperluan:</label>
+                    <input type="text" id="keperluan" name="keperluan">
+                </div>
+
+                <div class="form-row">
+                    <label for="tanggal_kunjungan">Tanggal Kunjungan:</label>
+                    <input type="date" id="tanggal_kunjungan" name="tanggal_kunjungan" required>
+                </div>
+
+                <button type="submit">Kirim</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
