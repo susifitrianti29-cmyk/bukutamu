bef3dff20f5d06fd6fcabe58e4472e1180b9eed9
susifitrianti29-cmyk <susifitrianti29@gmail.com>
2025-10-21 09:29:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,21 +10,25 @@
   </div>
 </div>
     <h2>Buku Tamu Digital 2025</h2>
-     <div class="container">
-        <div class="header">
      <form action="simpan.php" method="post" enctype="multipart/form-data">
-        Nama: <input type="text" name="nama" required><br>
+        <div class="form-row">
+         Nama: <input type="text" name="nama" required><br>
+            <div class="form-row">
         Instansi: <input type="text" name="instansi"><br>
+                <div class="form-row">
         Alamat: <input type="text" name="alamat"><br>
+                    <div class="form-row">
         No HP: <input type="text" name="no_hp"><br>
+                        <div class="form-row">
         Email: <input type="email" name="email"><br>
+                            <div class="form-row">
         Keperluan: <input type="text" name="keperluan"><br>
+                                <div class="form-row">
         Tanggal Kunjungan: <input type="date" name="tanggal_kunjungan" required><br>
         <!-- Tanda Tangan (upload gambar): <input type="file" name="ttd_digital" accept="image/*"><br>
         Foto (upload gambar): <input type="file" name="foto" accept="image/*"><br> -->
         <input type="submit" value="Kirim">
     </form>
         </div>
-    </div>         
 </body>
 </html>
