5e4ce4461de1dfca2b0abae75b04c9553d6787fc
susifitrianti29-cmyk <susifitrianti29@gmail.com>
2025-10-27 08:10:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "bukutamu");
+$koneksi = mysqli_connect("localhost", "root", "", "bukutamu_db");
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
