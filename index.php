61e85d78bbc738c4802b659a9b6f96ad01a56583
susifitrianti29-cmyk <susifitrianti29@gmail.com>
2025-11-03 11:28:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+    <div class="container">
     <div class="header-kominfo">
     <img src="cropped-cropped-cropped-logo-kominfo-1-300x300.png" alt="Logo Kominfo">
 
